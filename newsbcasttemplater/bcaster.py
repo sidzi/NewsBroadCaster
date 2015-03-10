10f47753621e06ee52c000d15f6b148d74e6ea72
Sid <rahulgupta388@gmail.com>
2015-03-10 16:24:20 +0530

    Frames from Video transferring :) , all sizes ;) . Yayyyyy !!! Finally

--- newsbcasttemplater/bcaster.py
+++ newsbcasttemplater/bcaster.py
@@ -7,17 +7,17 @@ import cv2
 TCP_IP = "localhost"
 TCP_PORT = 5000
 BUFFER_SIZE = 4096
-
+# noinspection PyArgumentList
+cap = cv2.VideoCapture("lmao.mp4")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-frame = cv2.imread("hello.png")
+ret, frame = cap.read()
 conn, addr = s.accept()
 print 'Connection address:', addr
 while True:
     data = pickle.dumps(frame)
-    datawithlength = str(len(data)) + ':::::' + data
-    print datawithlength
+    datawithlength = str(len(data)) + '::' + data
     conn.send(datawithlength)
     if str(conn.recv(BUFFER_SIZE)) == "FINISH":
         break

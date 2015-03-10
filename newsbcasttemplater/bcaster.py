9cbc8ebc43fbbb83b7c19c99a2ad0bd35918f9c7
Sid <rahulgupta388@gmail.com>
2015-03-10 21:10:33 +0530

    Video Stream to output file now working !!! Woohooo :D <Please Close Issue #1>

--- newsbcasttemplater/bcaster.py
+++ newsbcasttemplater/bcaster.py
@@ -12,14 +12,22 @@ cap = cv2.VideoCapture("lmao.mp4")
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
-ret, frame = cap.read()
 conn, addr = s.accept()
 print 'Connection address:', addr
-while True:
-    data = pickle.dumps(frame)
-    datawithlength = str(len(data)) + '::' + data
-    conn.send(datawithlength)
-    if str(conn.recv(BUFFER_SIZE)) == "FINISH":
-        break
 
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    data = datawithlength = ""
+    while True:
+        data = pickle.dumps(frame)
+        datawithlength = str(len(data)) + '::' + data
+        print str(len(data))
+        conn.send(datawithlength)
+        end_of_frame = str(conn.recv(BUFFER_SIZE))
+        if "FINFRAME" in end_of_frame:
+            print end_of_frame
+            break
+cap.release()
 conn.close()
--- newsbcasttemplater/bcastreciever.py
+++ newsbcasttemplater/bcastreciever.py
@@ -5,45 +5,49 @@ import cv2
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+fourcc = cv2.cv.CV_FOURCC(*'XVID')
+out = cv2.VideoWriter('out.avi', fourcc, 25.0, (320, 180))
+
 host = "localhost"
 port = 5000
 BUFFER_SIZE = 4096
 
 s.connect((host, port))
 i = 1
-length = None
-message = ""
+
 while True:
-    data = s.recv(BUFFER_SIZE)
-    if not data:
-        break
-    if length is None:
-        if '::' not in data:
+    length = None
+    message = ""
+    while True:
+        data = s.recv(BUFFER_SIZE)
+        if not data:
             break
+        if length is None:
+            if '::' not in data:
+                break
 
-        length_str, ignored, data = data.partition('::')
-        print "Size of the frame : " + length_str + "\n"
-        length = int(length_str)
+            length_str, ignored, data = data.partition('::')
+            print "\nSize of the frame : " + length_str
+            length = int(length_str)
 
-    if len(message) < length:
-        message += data
-        print "Appending ... " + str(i)
-        i += 1
-        if len(message) == length:
-            print "Received Status OK"
-            break
-        elif len(message) > length:
-            print "Recieved Status Overload"
-            break
-        else:
-            s.sendall("1")
-            continue
+        if len(message) < length:
+            message += data
+            if len(message) == length:
+                print "Received Status OK"
+                break
+            elif len(message) > length:
+                print "Recieved Status Overload"
+                break
+            else:
+                continue
 
-s.sendall("FINISH")
-s.close()
-frame = pickle.loads(message)
-print frame
-cv2.imshow('Recieved image', frame)
-cv2.waitKey(2500)
+    s.sendall(b'FINFRAME')
+    print "Now in frame : " + str(i)
+    frame = pickle.loads(message)
+    out.write(frame)
+    i += 1
+
 cv2.destroyAllWindows()
+s.close()
 

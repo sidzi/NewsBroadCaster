e45ddb701e2e830c23e96b1dd8cfc55cb740e3ed
Sid <rahulgupta388@gmail.com>
2015-03-30 00:02:49 +0530

    Add

---
+++ newsBcasterServer/videoWriter.py
@@ -0,0 +1,12 @@
+import cv2
+
+
+class videoWriter:
+    def __init__(self, vid_fps, vid_width, vid_height, filename="out", vid_format="mov"):
+        self.fourcc = cv2.cv.CV_FOURCC('m', 'p', '4', 'v')
+
+        self.out = cv2.VideoWriter(filename + '.' + vid_format, self.fourcc, float(vid_fps),
+                                   (int(float(vid_width)), int(float(vid_height))))
+
+    def writer(self, frame):
+        self.out.write(frame)
